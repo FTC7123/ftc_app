b7ca8707affb190d5c6972dba7262d2e6dc2c3a2
andrewLauer <andrewlauer1@gmail.com>
2017-07-24 19:38:54 -0500

    Worked on basic movements for Gearli

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/components/TMTRDriveControls.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/components/TMTRDriveControls.java
@@ -4,7 +4,9 @@ package org.firstinspires.ftc.teamcode.hardware.components;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 
+
 import org.firstinspires.ftc.teamcode.hardware.configurations.TwoMotorTankRobot;
+
 
 /**
  * Created by andre on 7/23/2017.
@@ -14,29 +16,50 @@ public class TMTRDriveControls extends TwoMotorTankRobot {
 
     TwoMotorTankRobot robot;
 
-    public void drive(double distance, double power) {
-
-        double movementTics;
-        movementTics = (distance / 10.16) * 1120;
+    public void drive (double distance){
+        distance =  distance * 4.1772973503558 * 1120;
 
         rightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         leftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
+        rightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        rightMotor.setTargetPosition((int)distance);
+        leftMotor.setTargetPosition((int)distance);
+
         rightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         leftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
-        rightMotor.setTargetPosition((int) movementTics);
-        leftMotor.setTargetPosition((int) movementTics);
+        rightMotor.setPower(0.75);
+        leftMotor.setPower(0.75);
 
-        rightMotor.getCurrentPosition();
-        leftMotor.getCurrentPosition();
+        while (rightMotor.isBusy() && leftMotor.isBusy() && opMode.opModeIsActive()){}
 
-        if ((movementTics - rightMotor.getCurrentPosition()) < 0) {
-            rightMotor.setPower(power);
-            leftMotor.setPower(power);
-        } else {
-            rightMotor.setPower(0);
-            leftMotor.setPower(0);
-        }
+        rightMotor.setPower(0);
+        leftMotor.setPower(0);
     }
+//    public void drive(double distance, double power) {
+//
+//        double targetTics;
+//        targetTics = (distance / 10.16) * 1120;
+//
+//        rightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+//        leftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+//
+//        rightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+//        leftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+//
+//        rightMotor.setTargetPosition((int) targetTics);
+//        leftMotor.setTargetPosition((int) targetTics);
+//
+//        while (robot.rightMotor.getCurrentPosition() < robot.rightMotor.getTargetPosition()){
+//            robot.rightMotor.setPower(power);
+//            robot.leftMotor.setPower(power);
+//        }
+//
+//        robot.rightMotor.setPower(power);
+//        robot.leftMotor.setPower(power);
+//
+//    }
 }
