4f518d96d4b7ca6974a334a3b67ecab236aedb62
Andrew Lauer <andrewlauer1@gmail.com>
2017-10-29 22:22:24 -0500

    Debugged the NullPointerException from the servo and worked on some more organization

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/configurations/LegolessRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/configurations/LegolessRobot.java
@@ -9,6 +9,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+import org.firstinspires.ftc.robotcore.internal.vuforia.VuforiaLocalizerImpl;
 
 /**
  * Created by andre on 9/24/2017.
@@ -20,9 +21,14 @@ public class LegolessRobot extends LinearOpMode {
 
     public int targetNumber = 0;
 
-    public Servo leftJewelServo;
-    public Servo rightJewelServo;
+//    public Servo leftJewelServo;
+//    public Servo rightJewelServo;
 
+    public Servo relicClawServo;
+
+
+
+    //public Servo relicArmServo;
 
     public void runOpMode() {
 
@@ -34,8 +40,12 @@ public class LegolessRobot extends LinearOpMode {
          * This is only for LegolessRobot specific hardware
          */
 
-        leftJewelServo = hardwareMap.servo.get("leftJewelServo");
-        rightJewelServo = hardwareMap.servo.get("rightJewelServo");
+//        leftJewelServo = hardwareMap.servo.get("leftJewelServo");
+//        rightJewelServo = hardwareMap.servo.get("rightJewelServo");
+
+        relicClawServo = hardwareMap.servo.get("relicClawServo");
+
+
 
 
 
@@ -49,42 +59,48 @@ public class LegolessRobot extends LinearOpMode {
          * This section handles the Vuforia section of the code.
          * Specifically just the idenification part, no navigation.
          */
-        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
+        try {
+            int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+            VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
 
-        parameters.vuforiaLicenseKey = "AZrVU7T/////AAAAGVo9hsImhE6RojK+5tOA/zEuh6SPnDmpFUC14U9v2xbapUtN8fWjT8/cjuJjqybmMknEdiy5uP153iKIS5Bh8NmtymZrpVxH92vqmR7tvtEV/i2VcZBI6rwd181sRIdgphcr/vm4Ow5MoxqhSsBqXYXdElfMiINTfv2riOQsnnTqtMzDo3ZRczpK4rOtqHuSJ4zqrQcP5wJiJXGYGEMzfyryC1i3bMQuwZ7EFIVpCRFilct/s+N27b+gjSMwmvaIXGfU/Mmv4XCGuUZPLEi3pbXKix98RGNfgD4+L9m8qejf3bc7fqq4k3EDunBxAJp7oGq3mzuOTnaEu2L65QujzAlqTNPyTNDZynZshmcyLFlj";
+            parameters.vuforiaLicenseKey = "AZrVU7T/////AAAAGVo9hsImhE6RojK+5tOA/zEuh6SPnDmpFUC14U9v2xbapUtN8fWjT8/cjuJjqybmMknEdiy5uP153iKIS5Bh8NmtymZrpVxH92vqmR7tvtEV/i2VcZBI6rwd181sRIdgphcr/vm4Ow5MoxqhSsBqXYXdElfMiINTfv2riOQsnnTqtMzDo3ZRczpK4rOtqHuSJ4zqrQcP5wJiJXGYGEMzfyryC1i3bMQuwZ7EFIVpCRFilct/s+N27b+gjSMwmvaIXGfU/Mmv4XCGuUZPLEi3pbXKix98RGNfgD4+L9m8qejf3bc7fqq4k3EDunBxAJp7oGq3mzuOTnaEu2L65QujzAlqTNPyTNDZynZshmcyLFlj";
 
-        parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
-        this.vuforia = ClassFactory.createVuforiaLocalizer(parameters);
+            parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
+            this.vuforia = ClassFactory.createVuforiaLocalizer(parameters);
 
-        VuforiaTrackables relicTrackables = this.vuforia.loadTrackablesFromAsset("RelicVuMark");
-        VuforiaTrackable relicTemplate = relicTrackables.get(0);
-        relicTemplate.setName("relicVuMarkTemplate");
+            VuforiaTrackables relicTrackables = this.vuforia.loadTrackablesFromAsset("RelicVuMark");
+            VuforiaTrackable relicTemplate = relicTrackables.get(0);
+            relicTemplate.setName("relicVuMarkTemplate");
 
-        waitForStart();
+            waitForStart();
 
-        relicTrackables.activate();
+            relicTrackables.activate();
 
-        while (opModeIsActive()){
-            RelicRecoveryVuMark vuMark = RelicRecoveryVuMark.from(relicTemplate);
-            if (vuMark != RelicRecoveryVuMark.UNKNOWN) {
-                telemetry.addData("Vumark", "%s visible", vuMark);
-                telemetry.update();
+            while (opModeIsActive()){
+                RelicRecoveryVuMark vuMark = RelicRecoveryVuMark.from(relicTemplate);
+                if (vuMark != RelicRecoveryVuMark.UNKNOWN) {
+                    telemetry.addData("Vumark", "%s visible", vuMark);
+                    telemetry.update();
 
-                /**
-                 * Sets the targetNumber depending on which target is visible.
-                 */
+                    /**
+                     * Sets the targetNumber depending on which target is visible.
+                     */
 
-                if (vuMark == RelicRecoveryVuMark.LEFT){
-                    targetNumber = 1;
-                } else if (vuMark == RelicRecoveryVuMark.CENTER){
-                    targetNumber = 2;
-                } else if (vuMark == RelicRecoveryVuMark.RIGHT){
-                    targetNumber = 3;
+                    if (vuMark == RelicRecoveryVuMark.LEFT){
+                        targetNumber = 1;
+                    } else if (vuMark == RelicRecoveryVuMark.CENTER){
+                        targetNumber = 2;
+                    } else if (vuMark == RelicRecoveryVuMark.RIGHT){
+                        targetNumber = 3;
+                    }
+                    break;
                 }
-                break;
             }
+        } catch (VuforiaLocalizerImpl.FailureException e){
+            e.printStackTrace();
+            throw e;
         }
+
     }
 }
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/LegolessTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/LegolessTeleOp.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.teleop;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.hardware.configurations.LegolessRobot;
 import org.firstinspires.ftc.teamcode.hardware.configurations.SixWheelDriveTrain;
@@ -8,19 +9,27 @@ import org.firstinspires.ftc.teamcode.hardware.configurations.SixWheelDriveTrain
 /**
  * Created by andre on 10/15/2017.
  */
-
+@TeleOp (name = "Legoless Teleop")
 public class LegolessTeleOp extends LegolessRobot{
     LinearOpMode opMode;
 
+
     @Override
     public void runOpMode() {
+        super.runOpMode();
         SixWheelDriveTrain driveTrain = new SixWheelDriveTrain();
         driveTrain.initialize(hardwareMap, this);
 
         waitForStart();
 
-        if (gamepad1.a){
-            leftJewelServo.setPosition(1);
+        sleep(2000);
+        try{
+            if(gamepad1.a){
+                relicClawServo.setPosition(0);
+            }
+        } catch (NullPointerException e) {
+            e.printStackTrace();
         }
+
     }
 }
