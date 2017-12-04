bf342c9bfe12cf17e3bfcacaf8b2cc494f050c0b
Andrew Lauer <andrewlauer1@gmail.com>
2017-12-03 20:22:44 -0600

    VCS Test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/GearliDemo.java
@@ -0,0 +1,46 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+
+/**
+ * Created by fello on 9/12/2017.
+ */
+@TeleOp(name="Gearli Demo")
+public class GearliDemo extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        Servo handServo;
+
+        handServo = hardwareMap.servo.get("handServo");
+
+        DcMotor rightMotor1 = hardwareMap.dcMotor.get("rightMotor1");
+        DcMotor rightMotor2 = hardwareMap.dcMotor.get("rightMotor2");
+        DcMotor leftMotor1 = hardwareMap.dcMotor.get("leftMotor1");
+        DcMotor leftMotor2 = hardwareMap.dcMotor.get("leftMotor2");
+
+        rightMotor1.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightMotor2.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        handServo.setPosition(0);
+
+        waitForStart();
+
+        while(opModeIsActive()){
+            rightMotor1.setPower(gamepad1.right_stick_y);
+            rightMotor2.setPower(gamepad1.right_stick_y);
+            leftMotor1.setPower(gamepad1.left_stick_y);
+            leftMotor2.setPower(gamepad1.left_stick_y);
+
+            if (gamepad1.a){
+                handServo.setPosition(0.75);
+            } else {
+                handServo.setPosition(0.25);
+            }
+        }
+    }
+}
