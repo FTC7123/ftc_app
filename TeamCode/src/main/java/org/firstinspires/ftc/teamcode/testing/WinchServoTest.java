5556f8c6e1c193816fee7ab52f307f2f7e5daf0a
Thomas Lauer <thomaslauer@users.noreply.github.com>
2017-12-30 10:03:51 -0600

    Added WinchServoTest

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/WinchServoTest.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode.testing;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.Servo;
+
+/**
+ * Created by thomaslauer on 12/28/17.
+ */
+@Autonomous(name = "WinchTest")
+public class WinchServoTest extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        Servo s = hardwareMap.servo.get("relicArmServo");
+        waitForStart();
+        while(opModeIsActive()) {
+            s.setPosition(0.1);
+            sleep(5000);
+            s.setPosition(0.2);
+            sleep(5000);
+            s.setPosition(0.3);
+            sleep(5000);
+            s.setPosition(0.4);
+            sleep(5000);
+            s.setPosition(0.5);
+            sleep(5000);
+            s.setPosition(0.6);
+            sleep(5000);
+            s.setPosition(0.7);
+            sleep(5000);
+            s.setPosition(0.8);
+            sleep(5000);
+            s.setPosition(0.9);
+            sleep(5000);
+        }
+    }
+}
